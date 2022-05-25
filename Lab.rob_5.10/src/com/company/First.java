3f54771e4055adfc1ea526606c788895641823dc
SashaYYYY <99317876+SashaYYYY@users.noreply.github.com>
2022-05-25 18:14:57 +0300

    Add files via upload

---
+++ Lab.rob_5.10/src/com/company/First.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class First {
+    public static void main(String[] args) {
+        int n = 500;
+        do {
+            n = n + 10;
+            System.out.println(n);
+        } while (n < 650);
+        while (n < 650) {
+            n = n + 10;
+            System.out.println(n);
+        }
+        for (; n < 650; n = n + 10) {
+            System.out.println(n);
+        }
+    }
+}
+
